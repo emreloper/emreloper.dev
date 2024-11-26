8b0bc912f5cd0417c01aafea7abd7b0e957c5421
emre-decrypt <emre@decryptmedia.com>
2024-11-26 23:28:12 +0100

    Remove article image from the listing page

--- src/components/Articles.tsx
+++ src/components/Articles.tsx
@@ -1,9 +1,7 @@
 import { ArticleMetadata } from '@/types/metadata';
-import Image from 'next/image';
 import Link from 'next/link';
 import fs from 'node:fs';
 import path from 'node:path';
-import Markdown from 'react-markdown';
 
 interface Article {
   slug: string;
@@ -45,37 +43,24 @@ export async function Articles() {
       <div className="mt-8 flex flex-col gap-y-16">
         {articles.map((article) => {
           const { metadata, slug } = article;
-          const { title, description, openGraph } = metadata;
+          const { title, openGraph } = metadata;
           const { publishedTime } = openGraph;
-          const image = openGraph.images[0];
 
           return (
             <article
               key={article.slug}
-              className="flex items-start gap-x-4 md:gap-x-8"
+              className="prose prose-slate dark:prose-invert grow"
             >
-              <div className="prose prose-slate dark:prose-invert grow">
-                <h3 className="text-slate-900 dark:text-white">
-                  <Link href={`/blog/${slug}`}>{title}</Link>
-                </h3>
-                <Markdown>{description}</Markdown>
-                <p>
-                  <time dateTime={publishedTime}>
-                    {new Date(publishedTime).toLocaleDateString('en-US', {
-                      dateStyle: 'long',
-                    })}
-                  </time>
-                </p>
-              </div>
-              <div className="relative hidden aspect-[1/1] w-40 flex-none md:block">
-                <Image
-                  className="object-cover"
-                  src={image.url.toString()}
-                  alt={image.alt ?? ''}
-                  sizes="160px"
-                  fill
-                />
-              </div>
+              <h3 className="text-slate-900 dark:text-white">
+                <Link href={`/blog/${slug}`}>{title}</Link>
+              </h3>
+              <p>
+                <time dateTime={publishedTime}>
+                  {new Date(publishedTime).toLocaleDateString('en-US', {
+                    dateStyle: 'long',
+                  })}
+                </time>
+              </p>
             </article>
           );
         })}
